95f0e0e5df9afc5ea5ef1b33a63805595fee4d54
user <connorbfranklin@gmail.com>
2015-04-08 09:31:01 -0400

    add green section headers

--- js/app.js
+++ js/app.js
@@ -5,38 +5,39 @@ $(function() {
     var router = new Grapnel();
 
     router.get('', function(req) {
-      resetSections()
-      //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa/' + window.location.hash);
+        resetSections()
+            //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa/' + window.location.hash);
     });
     router.get('/', function(req) {
-      resetSections()
-      //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa');
+        resetSections()
+            //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa');
     });
 
     router.get('/:id', function(req) {
         var id = req.params.id;
         console.log(id)
         viewSection(id)
-        //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa/' + window.location.hash);
+            //ga('send', 'pageview', 'http://gsa.gov/sustainablegsa/' + window.location.hash);
     });
 
     //clone items to thumbnails
     $('.splash > div').clone().each(function() {
         $(this).addClass('hidden')
+        $(this).find('.detail,.overview,h2').remove();
     }).appendTo('.thumbnails')
 
 
     $('.splash > div,.thumbnails > div').on('click', function() {
-      router.navigate('/'+$(this).attr('data-index'));
+        router.navigate('/' + $(this).attr('data-index'));
     })
 
 
-    function viewSection(id){
-      if(!$('.wrapper').hasClass('active')){
-        firstView();
-      }
-      $('.splash > div').removeClass('open');
-      $('.splash > [data-index=' + id + ']').addClass('open')
+    function viewSection(id) {
+        if (!$('.wrapper').hasClass('active')) {
+            firstView();
+        }
+        $('.splash > div').removeClass('open');
+        $('.splash > [data-index=' + id + ']').addClass('open')
     }
 
     function firstView() {
@@ -52,8 +53,9 @@ $(function() {
             thumbQueue[i] = setTimeout(thumbSlideIn, 1000 + i * 150, i)
         })
     }
-    function resetSections(){
-      $('.splash > div').removeClass('open');
-      $('.wrapper').removeClass('active')
+
+    function resetSections() {
+        $('.splash > div').removeClass('open');
+        $('.wrapper').removeClass('active')
     }
 })
