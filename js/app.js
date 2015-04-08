2eeafad6f62a4721f9cd2f431eddcbbad53adc3e
user <connorbfranklin@gmail.com>
2015-04-07 23:26:37 -0400

    added grapnel

--- js/app.js
+++ js/app.js
@@ -1,33 +1,55 @@
-$(function(){
-//clone items to thumbnails
-    $('.splash > div').clone().each(function(){
-      $(this).addClass('hidden')
+var router;
+
+$(function() {
+
+    var router = new Grapnel();
+
+    router.get('', function(req) {
+      resetSections()
+    });
+    router.get('/', function(req) {
+      resetSections()
+    });
+
+    router.get('/:id', function(req) {
+        var id = req.params.id;
+        console.log(id)
+        viewSection(id)
+    });
+
+    //clone items to thumbnails
+    $('.splash > div').clone().each(function() {
+        $(this).addClass('hidden')
     }).appendTo('.thumbnails')
 
 
-    //first selection
-    $('.splash > div').on('click',function(){
-      $(this).addClass('open').siblings().removeClass('open');
-      $(this).siblings().removeClass('open');
-      setTimeout(function(){
-        $('.wrapper').addClass('active')
-      },700);
-      //fade em in
-      var thumbQueue = [];
-      var thumbSlideIn = function(i){
-        $('.thumbnails > div').eq(i).removeClass('hidden').addClass(' slideInUp animated');
-      }
-      $('.thumbnails > div').each(function(i){
-        thumbQueue[i] = setTimeout(thumbSlideIn, 1000 + i * 150, i)
-      })
-    });
-
-    //subsequent selections
-    $('.thumbnails > div').on('click',function(){
-      $('.splash > div').removeClass('open');
-      var thumbIndex = $(this).attr('data-index')
-      console.log(thumbIndex)
-      $('.splash > [data-index='+thumbIndex+']').addClass('open')
-      $('.splash > [data-index='+thumbIndex+']').siblings().removeClass('open');
+    $('.splash > div,.thumbnails > div').on('click', function() {
+      router.navigate('/'+$(this).attr('data-index'));
     })
+
+
+    function viewSection(id){
+      if(!$('.wrapper').hasClass('active')){
+        firstView();
+      }
+      $('.splash > div').removeClass('open');
+      $('.splash > [data-index=' + id + ']').addClass('open')
+    }
+
+    function firstView() {
+        setTimeout(function() {
+            $('.wrapper').addClass('active')
+        }, 700);
+        //fade em in
+        var thumbQueue = [];
+        var thumbSlideIn = function(i) {
+            $('.thumbnails > div').eq(i).removeClass('hidden').addClass(' slideInUp animated');
+        }
+        $('.splash > div,.thumbnails > div').each(function(i) {
+            thumbQueue[i] = setTimeout(thumbSlideIn, 1000 + i * 150, i)
+        })
+    }
+    function resetSections(){
+      $('.splash > div').removeClass('open');
+    }
 })
