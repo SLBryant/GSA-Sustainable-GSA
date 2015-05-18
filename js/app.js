edc474a0bd045ebd1339c74ae5d708f4ff84c8b0
Adam Pflantzer <apflantzer@gmail.com>
2015-05-18 15:03:18 -0400

    remove extra router

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ if(!GSA){
 }
 
 $(function(){
-    var router = new Grapnel({root : '/git/sustainable-gsa/'  }); // change root for production / dev /git/sustainable-gsa/     
+    var router = new Grapnel({root : '/git/sustainable-gsa/'  }); // change root for production / dev /git/sustainable-gsa/
 
     GSA.initState = true;
     GSA.indexTracker;
@@ -291,9 +291,6 @@ $(function(){
 
     //Routes
 
-    router.get('', function(req) {
-        GSA.retrieveContent();
-    });
     router.get('/', function(req) {
         GSA.retrieveContent();
     });
