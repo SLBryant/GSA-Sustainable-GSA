bd66805edbcdfe018ee41c994fcd99891bea5b20
Adam Pflantzer <apflantzer@gmail.com>
2015-07-08 14:17:06 -0400

    change dummy text and opacity for overlay to .4

--- CID/strategically-sustainable.php
+++ CID/strategically-sustainable.php
@@ -17,8 +17,26 @@
             <td id="image">strategically-sustainable.jpg</td>
             <td id="tag">Strategically Sustainable</td>
             <td id="title">Strategically <br> Sustainable</td>
-            <td id="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis. Sed eu odio mi. Pellentesque ac leo interdum, tincidunt risus eget, accumsan risus. Etiam iaculis libero vel velit tincidunt, vitae egestas est tincidunt.</p></td>
-            <td id="content"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis. Sed eu odio mi. Pellentesque ac leo interdum, tincidunt risus eget, accumsan risus. Etiam iaculis libero vel velit tincidunt, vitae egestas est tincidunt. Vestibulum viverra sem et convallis consequat. Nullam ullamcorper dolor sapien, ac pretium leo vulputate nec. Donec fermentum interdum sapien vitae vehicula. Quisque neque diam, sagittis quis risus tincidunt, scelerisque auctor nulla. Sed convallis nisl sed porttitor elementum. Ut mauris orci, congue in felis eu, finibus semper augue. Nulla tellus orci, lobortis quis ipsum at, faucibus volutpat ex. Morbi vitae eros ac ante varius vulputate sit amet eget nibh. Vestibulum a ex sed velit posuere congue. Quisque ac ante sit amet orci consectetur finibus.</p></td>
+            <td id="excerpt"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis.</p></td>
+            <td id="content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis. Sed eu odio mi. Pellentesque ac leo interdum, tincidunt risus eget, accumsan risus. Etiam iaculis libero vel velit tincidunt, vitae egestas est tincidunt. Vestibulum viverra sem et convallis consequat. Nullam ullamcorper dolor sapien, ac pretium leo vulputate nec. ibus semper augue.Quisque ac ante sit amet orci consectetur finibus.</p>
+                <div class="overlay">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <article>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis.</p>
+                                <!--<span class="arrow-forward icon-arrow-right"></span>-->
+                            </article>
+                        </div>
+                        <div class="col-sm-6">
+                            <article>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis.</p>
+                                <!--<span class="arrow-forward icon-arrow-right"></span>-->
+                            </article>
+                        </div>
+                    </div>
+                </div>
+            </td>
         </tr>
     </tbody>
 </table>
