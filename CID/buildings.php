6ca0739d99240b516c09dda239f386ca9169e57e
natesulat <nate.sulat@ogilvy.com>
2015-08-19 14:32:25 -0400

    inserted blurbs in buildings
    want to make sure I haven’t messed everything up yet

--- CID/buildings.php
+++ CID/buildings.php
@@ -19,45 +19,84 @@
             <td id="tag">The Structure of Tomorrow</td>
             <td id="excerpt"><p>Managing over 9600 federal assets, we lead the way in sustainable building design, construction, retrofit, operations and maintenance.</p></td>
             <td id="content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis erat sapien. Aliquam ullamcorper viverra lacus non mattis. Sed eu odio mi. Pellentesque ac leo interdum, tincidunt risus eget, accumsan risus. Etiam iaculis libero vel velit tincidunt, vitae egestas est tincidunt. Vestibulum viverra sem et convallis consequat. Nullam ullamcorper dolor sapien, ac pretium leo vulputate nec. ibus semper augue.Quisque ac ante sit amet orci consectetur finibus.</p>
+                <img class="img-responsive" src="//placehold.it/1170x150?text=Placeholder" alt="Placeholder">
+                <p>With over 9600 federal assets under management, we pave the way in sustainable buildings for the government and private sector. From net-zero design, to green building certifications, to green roofs and advanced building technologies, GSA is proving every day that healthy and productive workplaces, a smaller environmental footprint, and reduced costs go hand in hand.</p>
                 <section id="columned-items">
                     <div class="row">
                         <article class="col-md-6">
                             <figure class="col-xs-2">
-                                <img class="img-responsive" src="images/icon-car-flower.png" alt="Buy Green Vehicles">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Buy Green Vehicles">
                             </figure>
                             <div class="col-xs-10">
-                                <h3>Buy Green Vehicles</h3>
-                                <p>GSA Fleet offers thousands of low-cost, high-efficiency vehicle options to help Federal agencies meet their missions sustainably.</p>
+                                <h3>Awards & Certifications</h3>
+                                <p>Explore our properties that have received green building certifications and awards. </p>
                             </div>
                         </article>
                         <article class="col-md-6">
                             <figure class="col-xs-2">
-                                <img class="img-responsive" src="images/icon-phone.png" alt="Alternate Fuel Vehicle (AFV) Tool">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Alternate Fuel Vehicle (AFV) Tool">
                             </figure>
                             <div class="col-xs-10">
-                                <h3>Alternate Fuel Vehicle (AFV) Tool</h3>
-                                <p>Helps agencies compare AFV options by size, price, fuel, availability, CO2 grams per mile, and lifetime GHG emissions. </p>
+                                <h3>Green Proving Ground</h3>
+                                <p>GSA’s Green Proving Ground evaluates pre- and early-commercial stage building technologies, accelerating the transition from bench-scale technology to commercial viability.</p>
                             </div>
                         </article>
                     </div>
                     <div class="row">
                         <article class="col-md-6">
                             <figure class="col-xs-2">
-                                <img class="img-responsive" src="images/icon-plug.png" alt="Electric Vehicle Pilots">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
                             </figure>
                             <div class="col-xs-10">
-                                <h3>Electric Vehicle Pilots</h3>
-                                <p>GSA has incorporated over 300 all-electric vehicles and 169 charging stations (and counting) into Federal fleets.</p>
+                                <h3>NetZero Design</h3>
+                                <p>By 2020, all new GSA buildings will be designed to achieve “net zero” energy, water and/or waste. Explore GSA and external resources related to net-zero design.</p>
                             </div>
                         </article>
                         <article class="col-md-6">
                             <figure class="col-xs-2">
-                                <img class="img-responsive" src="images/icon-car.png" alt="CNG Light Truck Initiative">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
                             </figure>
                             <div class="col-xs-10">
-                                <h3>CNG Light Truck Initiative</h3>
-                                <p>Agencies can acquire alternatively fueled pickup trucks and decrease dependence on foreign fuel sources. </p>
+                                <h3>SFTool</h3>
+                                <p>GSA’s comprehensive guide to sustainable buildings, SFTool offers user guides tailored to professional roles – taking you straight to the topics that interest you the most.</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                            <figure class="col-xs-2">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
+                            </figure>
+                            <div class="col-xs-10">
+                                <h3>Green Roofs</h3>
+                                <p>Green roofs slow stormwater runoff, provide excellent insulation, and reduce the “urban heat island” effect. Tour GSA’s extensive inventory of green roofs, including some of the largest in the U.S.</p>
+                            </div>
+                        </article>
+                        <article>
+                            <figure class="col-xs-2">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
+                            </figure>
+                            <div class="col-xs-10">
+                                <h3>Smart Buildings</h3>
+                                <p>GSA’s Smart Buildings strategy uses advanced data techniques to improve energy efficiency, increase tenant satisfaction, and share best practices.</p>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-6">
+                        <article>
+                            <figure class="col-xs-2">
+                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
+                            </figure>
+                            <div class="col-xs-10">
+                                <h3>Green Building Program Office</h3>
+                                <p>A number of GSA program offices contribute to our green buildings work, including:</p>
+                                <ul>
+                                    <li>Office of Federal High-Performance Green Buildings</li>
+                                    <li>Energy Program</li>
+                                    <li>Environment Program</li>
+                                    <li>Leasing Program</li>
+                                    <li>Office of Design and Construction</li>
+                                </ul>
                             </div>
                         </article>
                     </div>
