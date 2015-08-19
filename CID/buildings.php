e5d998597c74b10cb5fda42ff511246e1bdfdff1
natesulat <nate.sulat@ogilvy.com>
2015-08-19 14:50:35 -0400

    added row div to last three items of buildings

--- CID/buildings.php
+++ CID/buildings.php
@@ -1,109 +1,114 @@
 <body>
 
-<table id="data-block" data-size="6">
-    <thead>
-        <tr>
-            <th>Size</th>
-            <th>Background Image</th>
-            <th>Title</th>
-            <th>Tag</th>
-            <th>Excerpt</th>
-            <th>Content</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td id="size">3</td><!-- bootstrap grid -->
-            <td id="image">nav-buildings.jpg</td>
-            <td id="title">Buildings</td>
-            <td id="tag">The Structure of Tomorrow</td>
-            <td id="excerpt"><p>Managing over 9600 federal assets, we lead the way in sustainable building design, construction, retrofit, operations and maintenance.</p></td>
-            <td id="content">
-                <img class="img-responsive" src="//placehold.it/1170x150?text=Placeholder" alt="Placeholder">
-                <p>With over 9600 federal assets under management, we pave the way in sustainable buildings for the government and private sector. From net-zero design, to green building certifications, to green roofs and advanced building technologies, GSA is proving every day that healthy and productive workplaces, a smaller environmental footprint, and reduced costs go hand in hand.</p>
-                <section id="columned-items">
-                    <div class="row">
-                        <article class="col-md-6">
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Buy Green Vehicles">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>Awards & Certifications</h3>
-                                <p>Explore our properties that have received green building certifications and awards. </p>
+    <table id="data-block" data-size="6">
+        <thead>
+            <tr>
+                <th>Size</th>
+                <th>Background Image</th>
+                <th>Title</th>
+                <th>Tag</th>
+                <th>Excerpt</th>
+                <th>Content</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td id="size">3</td>
+                <!-- bootstrap grid -->
+                <td id="image">nav-buildings.jpg</td>
+                <td id="title">Buildings</td>
+                <td id="tag">The Structure of Tomorrow</td>
+                <td id="excerpt">
+                    <p>Managing over 9600 federal assets, we lead the way in sustainable building design, construction, retrofit, operations and maintenance.</p>
+                </td>
+                <td id="content">
+                    <img class="img-responsive" src="//placehold.it/1170x150?text=Placeholder" alt="Placeholder">
+                    <p>With over 9600 federal assets under management, we pave the way in sustainable buildings for the government and private sector. From net-zero design, to green building certifications, to green roofs and advanced building technologies, GSA is proving every day that healthy and productive workplaces, a smaller environmental footprint, and reduced costs go hand in hand.</p>
+                    <section id="columned-items">
+                        <div class="row">
+                            <article class="col-md-6">
+                                <figure class="col-xs-2">
+                                    <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Buy Green Vehicles">
+                                </figure>
+                                <div class="col-xs-10">
+                                    <h3>Awards & Certifications</h3>
+                                    <p>Explore our properties that have received green building certifications and awards. </p>
+                                </div>
+                            </article>
+                            <article class="col-md-6">
+                                <figure class="col-xs-2">
+                                    <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Alternate Fuel Vehicle (AFV) Tool">
+                                </figure>
+                                <div class="col-xs-10">
+                                    <h3>Green Proving Ground</h3>
+                                    <p>GSA’s Green Proving Ground evaluates pre- and early-commercial stage building technologies, accelerating the transition from bench-scale technology to commercial viability.</p>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="row">
+                            <article class="col-md-6">
+                                <figure class="col-xs-2">
+                                    <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
+                                </figure>
+                                <div class="col-xs-10">
+                                    <h3>NetZero Design</h3>
+                                    <p>By 2020, all new GSA buildings will be designed to achieve “net zero” energy, water and/or waste. Explore GSA and external resources related to net-zero design.</p>
+                                </div>
+                            </article>
+                            <article class="col-md-6">
+                                <figure class="col-xs-2">
+                                    <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
+                                </figure>
+                                <div class="col-xs-10">
+                                    <h3>SFTool</h3>
+                                    <p>GSA’s comprehensive guide to sustainable buildings, SFTool offers user guides tailored to professional roles – taking you straight to the topics that interest you the most.</p>
+                                </div>
+                            </article>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-6">
+                                <article>
+                                    <figure class="col-xs-2">
+                                        <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
+                                    </figure>
+                                    <div class="col-xs-10">
+                                        <h3>Green Roofs</h3>
+                                        <p>Green roofs slow stormwater runoff, provide excellent insulation, and reduce the “urban heat island” effect. Tour GSA’s extensive inventory of green roofs, including some of the largest in the U.S.</p>
+                                    </div>
+                                </article>
+                                <article>
+                                    <figure class="col-xs-2">
+                                        <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
+                                    </figure>
+                                    <div class="col-xs-10">
+                                        <h3>Smart Buildings</h3>
+                                        <p>GSA’s Smart Buildings strategy uses advanced data techniques to improve energy efficiency, increase tenant satisfaction, and share best practices.</p>
+                                    </div>
+                                </article>
                             </div>
-                        </article>
-                        <article class="col-md-6">
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Alternate Fuel Vehicle (AFV) Tool">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>Green Proving Ground</h3>
-                                <p>GSA’s Green Proving Ground evaluates pre- and early-commercial stage building technologies, accelerating the transition from bench-scale technology to commercial viability.</p>
+                            <div class="col-md-6">
+                                <article>
+                                    <figure class="col-xs-2">
+                                        <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
+                                    </figure>
+                                    <div class="col-xs-10">
+                                        <h3>Green Building Program Office</h3>
+                                        <p>A number of GSA program offices contribute to our green buildings work, including:</p>
+                                        <ul>
+                                            <li>Office of Federal High-Performance Green Buildings</li>
+                                            <li>Energy Program</li>
+                                            <li>Environment Program</li>
+                                            <li>Leasing Program</li>
+                                            <li>Office of Design and Construction</li>
+                                        </ul>
+                                    </div>
+                                </article>
                             </div>
-                        </article>
-                    </div>
-                    <div class="row">
-                        <article class="col-md-6">
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>NetZero Design</h3>
-                                <p>By 2020, all new GSA buildings will be designed to achieve “net zero” energy, water and/or waste. Explore GSA and external resources related to net-zero design.</p>
-                            </div>
-                        </article>
-                        <article class="col-md-6">
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>SFTool</h3>
-                                <p>GSA’s comprehensive guide to sustainable buildings, SFTool offers user guides tailored to professional roles – taking you straight to the topics that interest you the most.</p>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-6">
-                        <article>
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>Green Roofs</h3>
-                                <p>Green roofs slow stormwater runoff, provide excellent insulation, and reduce the “urban heat island” effect. Tour GSA’s extensive inventory of green roofs, including some of the largest in the U.S.</p>
-                            </div>
-                        </article>
-                        <article>
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="Electric Vehicle Pilots">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>Smart Buildings</h3>
-                                <p>GSA’s Smart Buildings strategy uses advanced data techniques to improve energy efficiency, increase tenant satisfaction, and share best practices.</p>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-md-6">
-                        <article>
-                            <figure class="col-xs-2">
-                                <img class="img-responsive" src="//placehold.it/200x200/63a723?text=Placeholder" alt="CNG Light Truck Initiative">
-                            </figure>
-                            <div class="col-xs-10">
-                                <h3>Green Building Program Office</h3>
-                                <p>A number of GSA program offices contribute to our green buildings work, including:</p>
-                                <ul>
-                                    <li>Office of Federal High-Performance Green Buildings</li>
-                                    <li>Energy Program</li>
-                                    <li>Environment Program</li>
-                                    <li>Leasing Program</li>
-                                    <li>Office of Design and Construction</li>
-                                </ul>
-                            </div>
-                        </article>
-                    </div>
-                </section>
-            </td>
-        </tr>
-    </tbody>
-</table>
+                        </div>
+                    </section>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 
 </body>
