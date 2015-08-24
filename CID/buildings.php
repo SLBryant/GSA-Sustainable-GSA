46855b55156367894493ba63d3849585df4340b3
natesulat <nate.sulat@ogilvy.com>
2015-08-24 14:46:19 -0400

    replaced icons with pngs

--- CID/buildings.php
+++ CID/buildings.php
@@ -30,7 +30,7 @@
                         <div class="row">
                             <article class="col-md-6">
                                 <figure class="col-xs-2">
-                                    <img class="img-responsive" src="images/buildings-certification.jpg" alt="Awards & Certifications Icon">
+                                    <img class="img-responsive" src="images/buildings-certification.png" alt="Awards & Certifications Icon">
                                 </figure>
                                 <div class="col-xs-10">
                                     <h3><a href="/portal/content/104927">Awards & Certifications</a></h3>
@@ -39,7 +39,7 @@
                             </article>
                             <article class="col-md-6">
                                 <figure class="col-xs-2">
-                                    <img class="img-responsive" src="images/buildings-gpg.jpg" alt="Green Proving Ground Icon">
+                                    <img class="img-responsive" src="images/buildings-gpg.png" alt="Green Proving Ground Icon">
                                 </figure>
                                 <div class="col-xs-10">
                                     <h3><a href="/portal/content/121935">Green Proving Ground</a></h3>
@@ -50,7 +50,7 @@
                         <div class="row">
                             <article class="col-md-6">
                                 <figure class="col-xs-2">
-                                    <img class="img-responsive" src="images/buildings-netzero.jpg" alt="NetZero Design Icon">
+                                    <img class="img-responsive" src="images/buildings-netzero.png" alt="NetZero Design Icon">
                                 </figure>
                                 <div class="col-xs-10">
                                     <h3><a href="/portal/content/181035">NetZero Design</a></h3>
@@ -59,7 +59,7 @@
                             </article>
                             <article class="col-md-6">
                                 <figure class="col-xs-2">
-                                    <img class="img-responsive" src="images/buildings-sftool.jpg" alt="SFTool Icon">
+                                    <img class="img-responsive" src="images/buildings-sftool.png" alt="SFTool Icon">
                                 </figure>
                                 <div class="col-xs-10">
                                     <h3><a href="https://sftool.gov/" target="_blank">SFTool</a></h3>
@@ -71,7 +71,7 @@
                             <div class="col-md-6">
                                 <article>
                                     <figure class="col-xs-2">
-                                        <img class="img-responsive" src="images/buildings-greenroofs.jpg" alt="Green Roofs Icon">
+                                        <img class="img-responsive" src="images/buildings-greenroofs.png" alt="Green Roofs Icon">
                                     </figure>
                                     <div class="col-xs-10">
                                         <h3><a href="/portal/content/166443">Green Roofs</a></h3>
@@ -80,7 +80,7 @@
                                 </article>
                                 <article>
                                     <figure class="col-xs-2">
-                                        <img class="img-responsive" src="images/buildings-smart.jpg" alt="Smart Buildings Icon">
+                                        <img class="img-responsive" src="images/buildings-smart.png" alt="Smart Buildings Icon">
                                     </figure>
                                     <div class="col-xs-10">
                                         <h3><a href="/portal/content/198217">Smart Buildings</a></h3>
@@ -91,7 +91,7 @@
                             <div class="col-md-6">
                                 <article>
                                     <figure class="col-xs-2">
-                                        <img class="img-responsive" src="images/buildings-programoffices.jpg" alt="Green Buildings Program Office Icon">
+                                        <img class="img-responsive" src="images/buildings-programoffices.png" alt="Green Buildings Program Office Icon">
                                     </figure>
                                     <div class="col-xs-10">
                                         <h3><a href="/portal/content/123747">Green Building Program Office</a></h3>
